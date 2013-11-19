44864c76ad95a4aacdb6a9164ee906f303d6b458
Laura A.G.G <alu0100698244@ull.edu.es>
2013-11-19 16:59:17 +0000

    Implementando matrix_disp

--- lib/mymatrix/matrix.rb
+++ lib/mymatrix/matrix.rb
@@ -33,9 +33,9 @@ class Matrix < Matrix_base
 		tmp="["
 		for i in 0...@ancho
 			tmp+="["
-			for j in 0...@ancho
+			for j in 0...@alto
 				tmp+="#{@matriz[i][j]}"
-				if j < (@ancho-1)
+				if j < (@alto-1)
 				tmp+=","
 				end	
 			end
@@ -47,17 +47,17 @@ class Matrix < Matrix_base
 
 	def ==(other)
 		for i in 0...@ancho
-			for j in 0...@ancho
+			for j in 0...@alto
 				@matriz[i][j]==other.matriz[i][j]
 			end
 		end
 	end
 
 	def +(other)
-		resultado=Matrix.new(@ancho)
-		raise 'Las matrices deben tener las mismas dimensiones' unless other.ancho==@ancho
+		resultado=Matrix.new(@ancho, @alto)
+		raise 'Las matrices deben tener las mismas dimensiones' unless other.ancho==@ancho && other.alto==@alto
 		for i in 0...@ancho
-			for j in 0...@ancho
+			for j in 0...@alto
 				resultado[i][j] = @matriz[i][j]+other.matriz[i][j]
 			end
 		end
@@ -65,13 +65,13 @@ class Matrix < Matrix_base
 	end
 
 	def *(other)
-        	resultado=Matrix.new(@ancho)
-		raise 'Las matrices deben tener las mismas dimensiones' unless other.ancho==@ancho
+        	resultado=Matrix.new(@ancho, @alto)
+		raise 'Las matrices deben tener las mismas dimensiones' unless other.alto==@ancho
 
 		for i in 0...@ancho do 
-                	for j in 0...@ancho do
+                	for j in 0...@alto do
                                 suma = (self.matriz[i][j] * other.matriz[j][0])
-                        	for k in 1...@ancho do
+                        	for k in 1...@alto do
                                 	resultado[i][k] = suma + (self.matriz[i][j] * other.matriz[j][k])
                         	end
                 	end
@@ -89,35 +89,113 @@ class Matrix_disp < Matrix_base
 	def initialize (ancho, alto, nnum)
 		super(ancho, alto)
 
-		@coord = Array.new[nnum]
-		@coord.map! {Array.new(2)}
-
 		@x = Array.new[nnum]
 		@y = Array.new[nnum]
 		@valor = Array.new[nnum]
-
+	end
+	
+	def encontrar(i, j)
+	  for k in 0...@x.length do
+		if ( @x[k]=i && @y[k]=j)
+		   return k; 
+		end
+ 	  end
+	  return -1
 	end
 
-	def [](i)
-                @coord[i]
-        end
-
-        def []=(i,other)
-                @coord[i]=other
-        end
 
 	def to_s
-		tmp="["
-		for i in 0...@ncho
-		for i2 in 0...@coord.size
-			tmp+="["
-			for j in 0...@alto
-			for j2 in 0
-			if @coord[i2][j2] = i and @coord[i2][j2+1] = j
-				tmp+="#{@valor[i]
-	
+                tmp="["
+                for i in 0...@ancho
+                        tmp+="["
+                        for j in 0...@alto
+				aux = encontrar(i,j)
+				if(aux != -1)
+                                	tmp+="#{@valor[aux]}"
+				else
+				  tmp+=0
+				end
+
+                                if j < (@alto-1)
+                                tmp+=","
+                                end
+                        end
+                        tmp+="]" 
+                end
+                tmp+="]"
+                tmp
+        end
+
+	def to_matrix
+		mat = Matrix.new(@ancho,@alto)
+
+		for i in 0...@ancho do
+		  for j in 0...@alto do
+		      aux = encontrar(i,j)
+		      if (aux != -1)
+			mat[i][j] = @valor[aux]
+		      else
+			mat[i][j] = 0
+		      end
+		  end
+		end
+		mat
+	end	
+
+	 def ==(other)
+                for i in 0...@x.length
+                   @x[k] == other.x[k]
+		   @y[k] == other.y[k]
+		   @valor[k] == other.valor[k]
+                end
+        end
+
+        def +(other)
+                resultado=Matrix.new(@ancho, @alto)
+                raise 'Las matrices deben tener las mismas dimensiones' unless other.ancho==@ancho && other.alto==@alto
+                for i in 0...@ancho
+                        for j in 0...@alto
+                	  aux1 = encontrar(i,j)
+			  aux2 = other.encontrar(i,j)
+
+			  if(aux1 == -1)
+				aux1 = 0
+			  else
+				aux1 = @valor[aux1]
+			  end
+			  
+			  if(aux2 == -1)
+			  	aux2 = 0
+			  else
+				aux2 = other.valor[aux2]
+			  end
+
+			  resultado[i][j] = aux1 + aux2
+                        end
+                end
+                resultado
+        end
+
+        def *(other)
+
+		m2 = other.to_matrix
+		m1 = self.to_matrix
+
+                resultado=Matrix.new(@ancho, @alto)
+                raise 'Las matrices deben tener las mismas dimensiones' unless other.alto==@ancho
+
+                for i in 0...@ancho do
+                        for j in 0...@alto do
+                                suma = (m1.matriz[i][j] * m2.matriz[j][0])
+                                for k in 1...@alto do
+                                        resultado[i][k] = suma + (m1.matriz[i][j] * m2.matriz[j][k])
+                                end
+                        end
+                end
+                resultado
+        end
+
 
 end
-
 
 
