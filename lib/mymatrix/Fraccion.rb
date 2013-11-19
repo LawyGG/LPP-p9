45271b89c3afb4e8c6899eebad3b6c7a63004e5d
Laura A.G.G <alu0100698244@ull.edu.es>
2013-11-19 14:44:50 +0000

    Incluyendo matrix y fraccion

---
+++ lib/mymatrix/Fraccion.rb
@@ -0,0 +1,95 @@
+
+class Fraccion
+  
+  attr_accessor :num, :denom
+  include Comparable
+
+  def initialize(n, d)
+    @num, @denom = n, d
+    
+    raise TypeError unless n.is_a?(Numeric)
+    raise TypeError unless d.is_a?(Numeric)
+    raise TypeError unless (d != 0)
+    
+    min = mcd(@num,@denom)
+    @num = @num / min
+    @denom = @denom / min
+  end
+  
+  def <=> (other)
+    nd = mcm(@denom,other.denom)
+    nn1 = (nd/@denom)*@num
+    nn2 = (nd/other.denom)*other.num
+
+    nn1 <=> nn2
+  end 
+
+  def mcd(a,b)
+    a, b = a.abs, b.abs
+    while b != 0
+      a, b = b, a % b
+    end
+    a
+  end
+
+  def mcm(a,b)
+    l1 = a*b
+    l2 = l1/mcd(a,b) 
+  end
+
+  def to_s
+    "#{@num}/#{@denom}"
+  end
+
+  def to_f
+    (@num.to_f)/(@denom.to_f)
+  end
+
+  def abs
+    Fraccion.new(@num.abs,@denom.abs)
+  end
+
+  def reciproco
+    Fraccion.new(@denom,@num)
+  end
+
+  def opuesto
+    Fraccion.new((-1)*@num,@denom)
+  end
+
+  def %(other)
+    Fraccion.new(0,1)
+  end
+  
+  def coerce(something)
+    [self,something]
+  end
+  
+  def +(other)
+    nd = mcm(@denom,other.denom)
+    nn1 = (nd/@denom)*@num
+    nn2 = (nd/other.denom)*other.num
+    nn = nn1 + nn2
+    Fraccion.new(nn,nd)
+  end
+
+  def -(other)
+    nd = mcm(@denom,other.denom)
+    nn1 = (nd/@denom)*@num
+    nn2 = (nd/other.denom)*other.num
+    nn = nn1 - nn2
+    Fraccion.new(nn,nd)
+  end
+
+  def *(other)
+    nn = (@num*other.num)
+    nd = (@denom*other.denom)
+    Fraccion.new(nn,nd)
+  end
+
+  def /(other)
+    nn = (@num*other.denom)
+    nd = (@denom*other.num)
+    Fraccion.new(nn,nd)
+  end
+end
